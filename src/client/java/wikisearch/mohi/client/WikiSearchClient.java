fd727ce636d9b6ded2b1622f5ab11ede756965c6
mohi0321 <benmohi121@gmail.com>
2026-06-01 22:02:32 +0200

    Fix split sourcesets: Move client classes to src/client/java

--- src/client/java/wikisearch/mohi/client/WikiSearchClient.java
+++ src/client/java/wikisearch/mohi/client/WikiSearchClient.java
@@ -1,10 +1,31 @@
 package wikisearch.mohi.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import wikisearch.mohi.screen.WikiBookScreen;
 
 public class WikiSearchClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    private static KeyBinding wikiKeyBinding;
+
+    @Override
+    public void onInitializeClient() {
+        wikiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.wikisearch.open",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_K,
+                "category.wikisearch.keys"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (wikiKeyBinding.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.setScreen(new WikiBookScreen());
+                }
+            }
+        });
+    }
 }
